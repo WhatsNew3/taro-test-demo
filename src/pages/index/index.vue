b74bf0dd4ae3cb9dae092d68e88dff409ab04e69
Fangjianyu <fangjianyu_1997@yeah.net>
2024-10-13 17:30:30 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <view v-html="msg"></view>
+  </view>
+</template>
+
+<script>
+import {ref} from 'vue'
+import './index.scss'
+
+export default {
+  setup() {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
